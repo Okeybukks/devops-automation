ef20df372cc78ec23cea80e16bf779ddfd1edc27
Okeybukks <achebepeter94@gmail.com>
2023-06-07 13:57:57 +0000

    test

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "eu-east-1"
+  region = "us-east-1"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
 
 terraform {
   backend "s3" {
