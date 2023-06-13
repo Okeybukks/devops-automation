83a325bbe407126420add30f53a38220eeafa59b
Okeybukks <achebepeter94@gmail.com>
2023-06-13 15:05:30 +0100

    test

--- terraform/iamrole.tf
+++ terraform/iamrole.tf
@@ -55,3 +55,13 @@ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRC
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
   role       = aws_iam_role.workernodes.name
 }
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.workernodes.name
+}
