a98195cf79fab685e9c45da1af698d7333f2cd88
Okeybukks <achebepeter94@gmail.com>
2023-06-13 22:58:03 +0100

    test

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -7,6 +7,8 @@ resource "aws_eks_cluster" "cluster" {
 
     # subnet_ids = [aws_subnet.subnets[count.index].id, for count.index in range(length(var.subnet_cidrs))]
     subnet_ids         = [aws_subnet.public_subnet[0].id, aws_subnet.public_subnet[1].id, aws_subnet.private_subnet[0].id, aws_subnet.private_subnet[1].id, ]
+    endpoint_private_access = true
+    endpoint_public_access  = true
     security_group_ids = [aws_security_group.eks-cluster.id]
   }
   depends_on = [
